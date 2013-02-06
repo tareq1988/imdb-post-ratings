3f1a5cec274d4464d9183e08e057f096257b3c88
Tareq Hasan <tareq1988@gmail.com>
2013-02-06 19:07:05 +0600

    initial commit

---
+++ base-plugin.php
@@ -0,0 +1,148 @@
+<?php
+/*
+Plugin Name: IMDB Post Ratings
+Plugin URI: http://wedevs.com/
+Description: Description
+Version: 0.1
+Author: Tareq Hasan
+Author URI: http://tareq.wedevs.com/
+License: GPL2
+*/
+
+/**
+ * Copyright (c) 2013 Tareq Hasan (email: tareq@wedevs.com). All rights reserved.
+ *
+ * Released under the GPL license
+ * http://www.opensource.org/licenses/gpl-license.php
+ *
+ * This is an add-on for WordPress
+ * http://wordpress.org/
+ *
+ * **********************************************************************
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ * **********************************************************************
+ */
+
+// don't call the file directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+/**
+ * IMDB_Post_Ratings class
+ *
+ * @class IMDB_Post_Ratings The class that holds the entire IMDB_Post_Ratings plugin
+ */
+class IMDB_Post_Ratings {
+
+    /**
+     * Constructor for the IMDB_Post_Ratings class
+     *
+     * Sets up all the appropriate hooks and actions
+     * within our plugin.
+     *
+     * @uses register_activation_hook()
+     * @uses register_deactivation_hook()
+     * @uses is_admin()
+     * @uses add_action()
+     */
+    public function __construct() {
+        register_activation_hook( __FILE__, array( $this, 'activate' ) );
+        register_deactivation_hook( __FILE__, array( $this, 'deactivate' ) );
+
+        // Localize our plugin
+        add_action( 'init', array( $this, 'localization_setup' ) );
+
+        // Loads frontend scripts and styles
+        add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+
+    }
+
+    /**
+     * Initializes the IMDB_Post_Ratings() class
+     *
+     * Checks for an existing IMDB_Post_Ratings() instance
+     * and if it doesn't find one, creates it.
+     */
+    public static function init() {
+        static $instance = false;
+
+        if ( ! $instance ) {
+            $instance = new IMDB_Post_Ratings();
+        }
+
+        return $instance;
+    }
+
+    /**
+     * Placeholder for activation function
+     *
+     * Nothing being called here yet.
+     */
+    public function activate() {
+
+    }
+
+    /**
+     * Placeholder for deactivation function
+     *
+     * Nothing being called here yet.
+     */
+    public function deactivate() {
+
+    }
+
+    /**
+     * Initialize plugin for localization
+     *
+     * @uses load_plugin_textdomain()
+     */
+    public function localization_setup() {
+        load_plugin_textdomain( 'baseplugin', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+    }
+
+    /**
+     * Enqueue admin scripts
+     *
+     * Allows plugin assets to be loaded.
+     *
+     * @uses wp_enqueue_script()
+     * @uses wp_localize_script()
+     * @uses wp_enqueue_style
+     */
+    public function enqueue_scripts() {
+
+        /**
+         * All styles goes here
+         */
+        wp_enqueue_style( 'ipr-styles', plugins_url( 'css/style.css', __FILE__ ), false, date( 'Ymd' ) );
+
+        /**
+         * All scripts goes here
+         */
+        wp_enqueue_script( 'ipr-scripts', plugins_url( 'js/script.js', __FILE__ ), array( 'jquery' ), false, true );
+
+
+        /**
+         * Example for setting up text strings from Javascript files for localization
+         *
+         * Uncomment line below and replace with proper localization variables.
+         */
+        // $translation_array = array( 'some_string' => __( 'Some string to translate', 'baseplugin' ), 'a_value' => '10' );
+        // wp_localize_script( 'base-plugin-scripts', 'baseplugin', $translation_array ) );
+
+    }
+
+} // IMDB_Post_Ratings
+
+$imdb_ratings = IMDB_Post_Ratings::init();
